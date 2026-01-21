9d99234d549b066ba40a145963bea023f8d7dcad
daniel48saigua <dfsaigua@gmail.com>
2026-01-21 14:07:44 +0000

    ejercicios bucle

--- ejercicios/nivelBasico.js
+++ ejercicios/nivelBasico.js
@@ -17,4 +17,27 @@ function numeroRandom(){
     return result2;
 }
 
-console.log(numeroRandom());
+//console.log(numeroRandom());
+
+
+/*function parEimpar(n){
+    residuo=n%2
+    if (residuo==0)
+        console.log('PAR')
+    else
+        console.log('IMPAR')
+
+}
+
+parEimpar(0)*/
+
+
+
+function imprimirAlreves(cadena){
+    for(i=cadena.length-1;i>=0;i--)
+        console.log(cadena[i])
+        
+
+}
+
+imprimirAlreves('daniel')
