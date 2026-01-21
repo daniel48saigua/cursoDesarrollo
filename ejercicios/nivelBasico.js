afd778632b0177aa4ca09201bc71bd00af2d64df
daniel48saigua <dfsaigua@gmail.com>
2026-01-21 13:29:16 +0000

    Ejercicios bàsicos

---
+++ ejercicios/nivelBasico.js
@@ -0,0 +1,20 @@
+function cubo(a){
+    const result=Math.pow(a,3)
+    return result;
+}
+
+//console.log(cubo(4))
+
+function rectangulo(b,c){
+    const result1=b*c
+    return result1;
+}
+
+//console.log(rectangulo(3,4))
+
+function numeroRandom(){
+    result2=Math.floor(Math.random()*11)
+    return result2;
+}
+
+console.log(numeroRandom());
